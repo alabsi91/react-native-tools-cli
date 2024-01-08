759e3c196533cd5cd9adbc210e3d6a6f35c72703
Ahmed ALAbsi <alabsi91@gmail.com>
2024-01-08 21:23:43 +0300

    Remove `printHelpMessage`

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -1,33 +1,3 @@
-import { printHelpMessage } from '@cli';
-
 export default function printHelp() {
-  printHelpMessage({
-    scriptName: 'node-cli',
-    description: 'a template to create an awesome cli with nodejs',
-    optionsList: [
-      {
-        flags: '-h, --help',
-        description: 'Output usage information',
-      },
-      {
-        flags: '--full-name=string',
-        description: 'To enter your name',
-      },
-      {
-        flags: '--age=number',
-        description: 'To enter your age',
-      },
-    ],
-    examples: ['node-cli --full-name="John Doe" --age=100', 'node-cli -h'],
-    positionalArguments: [
-      {
-        name: 'input',
-        description: 'The input file',
-      },
-      {
-        name: 'output',
-        description: 'The output file',
-      },
-    ],
-  });
+  console.log('Please Implement me');
 }
