87684e3d76ec9bb5dd9a803d53b280a0551d1ddb
Ahmed ALAbsi <alabsi91@gmail.com>
2024-01-27 16:08:52 +0300

    Fix parse

--- src/cli-tools/commandSchema/parseSchema.ts
+++ src/cli-tools/commandSchema/parseSchema.ts
@@ -61,8 +61,9 @@ function parseArguments(schema: CommandSchema[]) {
     if (key !== null) {
       if (value === null) continue;
 
-      if (results.command && isOptionAlias(results.command, key)) {
-        const option = optionAliasToOption(results.command, key);
+      const searchInCommand = results.command ?? NO_COMMAND;
+      if (isOptionAlias(searchInCommand, key)) {
+        const option = optionAliasToOption(searchInCommand, key);
         if (option) results[option] = value;
         syntax.push('option');
         continue;
